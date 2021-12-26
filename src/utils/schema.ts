f9b6385f1a72cf5fecc47b3282d540359f9d3a79
kir3kin <kir3kin@gmail.com>
2021-12-26 16:55:52 +0100

    resolve conflict

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -22,11 +22,7 @@ export const validationSchema = yup.object().shape({
 		})
 		.test('dimensions', HELPER_TEXT.photo.dimensions, async (value) => {
 			if (!value[0]) return true
-<<<<<<< HEAD
 			else if (value[0].type !== 'image/jpeg') return false// 'cause doesn't react on the previos test option | plugin's bug
-=======
-			else if (value[0].type !== 'image/jpeg') return false// 'cause doesn't react on the previos test option | plugin bug
->>>>>>> 8786f254a37a925e0a4f30f403a7a7bcf6cbb053
 			return await validateUploadedFile(value[0])
 		})
 })
